65ff8db3e8aae9fa498cdc467abd66009500ad0e
Nikhil  Pachange <nikhil1pachange@gmail.com>
2025-09-23 20:17:42 +0530

    Update clean.js

--- grunt_options/clean.js
+++ grunt_options/clean.js
@@ -1,32 +1,30 @@
 /**
- *
- * Module: grunt-contrib-clean
- * Documentation: https://npmjs.org/package/grunt-contrib-clean
- *
+ * Grunt Task: Clean
+ * Reference: https://npmjs.org/package/grunt-contrib-clean
  */
 
 module.exports = {
-	coreIconsStart: [
+	iconCleanupTemp: [
 		'<%= pkg._component_path %>/theme/icons/bigcommerce',
 		'<%= pkg._bc_public_fonts_path %>icons-bigcommerce',
 		'<%= pkg._bc_public_pcss_path %>base/_icons.pcss',
 		'<%= pkg._bc_public_pcss_path %>utilities/variables/_icons.pcss',
 	],
 
-	coreIconsEnd: [
+	iconCleanupZip: [
 		'<%= pkg._component_path %>/bigcommerce-icons.zip',
 	],
 
-	themeMinCSS: [
+	cssDistClean: [
 		'<%= pkg._bc_css_dist_path %>*.css',
 	],
 
-	themeMinJS: [
+	jsDistClean: [
 		'<%= pkg._bc_public_js_dist_path %>*.min.js',
 		'<%= pkg._bc_admin_js_dist_path %>*.min.js',
 	],
 
-	themeMinVendorJS: [
+	vendorBundleClean: [
 		'<%= pkg._bc_public_js_dist_path %>vendorWebpack.min.js',
 	],
 };
